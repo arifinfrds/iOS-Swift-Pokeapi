7bbbf6f41247f78169d0f8ee2c55d647eea0766e
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-08-18 13:15:07 +0700

    Initial Commit

---
+++ Pokeapi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokeapi
+//
+//  Created by Arifin Firdaus on 18/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
