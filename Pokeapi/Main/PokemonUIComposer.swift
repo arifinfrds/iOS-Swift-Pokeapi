37d45279c4b6d16979515b628e45a44ab73bb6bf
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-08-18 23:38:52 +0700

    Simplify creation of components for the composer

--- Pokeapi/Main/PokemonUIComposer.swift
+++ Pokeapi/Main/PokemonUIComposer.swift
@@ -29,28 +29,38 @@ final class PokemonUIComposer {
     
     static func pokemonListComposedWith() -> PokemonsViewController {
         
-        let localDataSource = DefaultPokemonLocalDataSource(
-            cacheClient: UserDefaultCacheClient(defaults: UserDefaults.standard)
-        )
-        let remoteDataSource = DefaultPokemonRemoteDataSource(
-            httpClient: URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        )
-        
-        let useCase: LoadPokemonsUseCase = CacheableLoadPokemonsFromRemoteUseCase(
-            loadPokemonFromRemoteUseCase: LoadPokemonsLocalFirstUseCase(
-                localUseCase: LoadPokemonsFromLocalUseCase(localDataSource: localDataSource),
-                remoteUseCase: LoadPokemonsFromRemoteUseCase(remoteDataSource: remoteDataSource)
-            ),
-            pokemonCache: localDataSource
-        )
-        
-        let presenter = PokemonsPresenter(useCase: useCase)
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: .main)
-        let identifier = "PokemonsViewController"
-        let viewController = storyboard.instantiateViewController(identifier: identifier) { coder in
-            return PokemonsViewController(coder: coder, presenter: presenter)
+        let createPresenter: () -> PokemonsPresenter = {
+            let localDataSource = DefaultPokemonLocalDataSource(
+                cacheClient: UserDefaultCacheClient(defaults: UserDefaults.standard)
+            )
+            let remoteDataSource = DefaultPokemonRemoteDataSource(
+                httpClient: URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+            )
+            
+            let useCase: LoadPokemonsUseCase = CacheableLoadPokemonsFromRemoteUseCase(
+                loadPokemonFromRemoteUseCase: LoadPokemonsLocalFirstUseCase(
+                    localUseCase: LoadPokemonsFromLocalUseCase(localDataSource: localDataSource),
+                    remoteUseCase: LoadPokemonsFromRemoteUseCase(remoteDataSource: remoteDataSource)
+                ),
+                pokemonCache: localDataSource
+            )
+            
+            let presenter = PokemonsPresenter(useCase: useCase)
+            return presenter
         }
+        
+        let presenter = createPresenter()
+        
+        let createView: () -> PokemonsViewController = {
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            let identifier = "PokemonsViewController"
+            let viewController = storyboard.instantiateViewController(identifier: identifier) { coder in
+                return PokemonsViewController(coder: coder, presenter: presenter)
+            }
+            return viewController
+        }
+        
+        let viewController = createView()
         
         presenter.view = viewController
         
