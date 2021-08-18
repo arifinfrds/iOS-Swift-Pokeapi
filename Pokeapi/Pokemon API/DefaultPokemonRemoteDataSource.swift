53f140480c9990f748c0987d0164fd6a2a534e80
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-08-18 14:42:04 +0700

    Extract URLSession implementation detail from remote data source implementation class into a component `URLSessionHTTPClient` and its protocol (``HTTPClient`) to enable flexibility (pholimorphic HTTPCLient) in the future

---
+++ Pokeapi/Pokemon API/DefaultPokemonRemoteDataSource.swift
@@ -0,0 +1,66 @@
+//
+//  DefaultPokemonRemoteDataSource.swift
+//  Pokeapi
+//
+//  Created by Arifin Firdaus on 18/08/21.
+//
+
+import Foundation
+
+protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (Result<Data, Error>) -> Void)
+}
+
+final class URLSessionHTTPClient: HTTPClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        session.dataTask(with: url) { data, urlResponse, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let _ = urlResponse as? HTTPURLResponse, let receivedData = data {
+                completion(.success(receivedData))
+            } else {
+                let error = NSError(domain: "\(type(of: DefaultPokemonRemoteDataSource.self))", code: 1, userInfo: [:])
+                completion(.failure(error))
+            }
+        }
+        .resume()
+    }
+}
+
+final class DefaultPokemonRemoteDataSource: PokemonRemoteDataSource {
+    
+    private let httpClient: HTTPClient
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    func loadPokemons(completion: @escaping (LoadPokemonUseCase.Result) -> Void) {
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+        httpClient.get(from: url) { result in
+            switch result {
+            case .success(let data):
+                let decoder = JSONDecoder()
+                do {
+                    let decoded = try decoder.decode(LoadPokemonResponse.self, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            
+        }
+    }
+}
